70a106e954bc09dcd27ae8f0d12d1014e49e6de9
hmparsons-sde <holly-parsons@outlook.com>
2021-12-13 20:29:57 -0600

    models etc

---
+++ Controllers/EpisodeController.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCoreVueStarter.DataAccess;
+using AspNetCoreVueStarter.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreVueStarter.Controllers
+{
+    [Route("api/Episodes")]
+    [ApiController]
+    public class EpisodeController : Controller
+    {
+        EpisodeRepository _repo;
+        public EpisodeController()
+        {
+            _repo = new EpisodeRepository();
+        }
+
+        [HttpGet]
+        public List<Episode> GetEpisodes()
+        {
+            return _repo.GetAllEpisodes();
+        }
+    }
+}
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -28,6 +28,37 @@ namespace AspNetCoreVueStarter.Controllers
             return (List<User>)_repo.GetAll();
         }
 
+        //[HttpGet("{Id}")]
+        //public IActionResult GetUserById(Guid UserId)
+        //{
+        //    var user = _repo.GetSingleUserById(UserId);
+
+        //    //if (user == null)
+        //    //{
+        //    //    return NotFound($"No user with the id {UserId} was found.");
+        //    //}
+
+        //    return Ok((IEnumerable<User>)user);
+        //}
+
+        ////private IActionResult NotFound(string v)
+        ////{
+        ////    throw new NotImplementedException();
+        ////}
+
+        //[HttpGet("authedUsers/{FirebaseKey}")]
+        //public IActionResult GetUserByFBKey(string FirebaseKey)
+        //{
+        //    var user = _repo.GetSingleUserByFBKey(FirebaseKey);
+
+        //    //if (user == null)
+        //    //{
+        //    //    return NotFound($"No user with the id {FirebaseKey} was found.");
+        //    //}
+
+        //    return Ok((IEnumerable<User>)user);
+        //}
+
         private IActionResult Ok(IEnumerable<User> enumerable)
         {
             throw new NotImplementedException();
