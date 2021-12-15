c9246a59093dc35a13aebab3501446c5ee1c6c38
hmparsons-sde <holly-parsons@outlook.com>
2021-12-15 11:34:07 -0600

    tailwind css

---
+++ ClientApp/src/components/sidenav/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+    <menu-items />
+
+  </div>
+</template>
+
+<script>
+import menuItems from './MenuItems.vue';
+export default {
+  components: {
+    menuItems,
+  },
+};
+</script>
